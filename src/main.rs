63dfe751f70d934fbe91412dce0b864b5cac7ebe
Nima Karimian Kakolaki <niimak97@gmail.com>
2026-03-29 17:27:28 +0200

    error handling and SOC

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,41 @@
-use std::{env, fs};
+use std::{env, error::Error, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    // let query = args.get(1).unwrap();
-    // let filename = args.get(2).unwrap();
-    let _query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|error| {
+        println!("Problem parsing arguments: \n{error}");
+        process::exit(1);
+    });
 
-    println!("In file {file_path}");
-    let contents = fs::read_to_string(file_path).expect("Couldn't read the file");
+    let (query, file_path) = (&config.query, &config.file_path);
+    println!("In file {file_path} looking for {query}");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(&config.file_path)?;
     println!("With text:\n{contents}");
+    Ok(())
+}
+struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err(
+                "Not enough arguments provided.\n\tUsage: cargo run -- <query> <file_path>",
+            );
+        }
+        Ok(Config::new(args))
+    }
+
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config { query, file_path }
+    }
 }

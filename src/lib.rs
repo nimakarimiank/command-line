ce43c83372aaf1d0d2ef9c49d7e1cbc3182f55ee
Nima Karimian Kakolaki <niimak97@gmail.com>
2026-03-29 23:31:21 +0200

    Fix formatting in test case for search function

--- src/lib.rs
+++ src/lib.rs
@@ -14,9 +14,8 @@ mod tests {
     #[test]
     fn one_result() {
         let query = "duct";
-        let contents = "\
-        Rust:
-        safe, fast, productive.
+        let contents = r"\Rust:
+safe, fast, productive.
         Pick three.";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }

36985ec795f48fcfd5943be7a04f482d9e0c2ba0
Nima Karimian Kakolaki <niimak97@gmail.com>
2026-03-29 17:52:26 +0200

    Implement search functionality and update main to utilize it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use herogrep::search;
 use std::{env, error::Error, fs, process};
 
 fn main() {
@@ -16,7 +17,10 @@ fn main() {
 }
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.file_path)?;
-    println!("With text:\n{contents}");
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
     Ok(())
 }
 struct Config {
